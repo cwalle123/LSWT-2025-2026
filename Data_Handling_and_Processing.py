2ac73b7792c84fc620c430ec245041303803ae3d
cwalle123 <wcliftonjohn@gmail.com>
2025-11-25 13:06:32 +0100

    Create Data_Handling_and_Processing.py

---
+++ Data_Handling_and_Processing.py
@@ -0,0 +1,22 @@
+"""This file is used to import, export, and save all revelant data for easy access in other files"""
+
+####################################################################################################
+
+# External imports
+import numpy as np
+
+# Internal imports
+
+####################################################################################################
+"""Functions"""
+
+# ADD FUNCTIONS HERE
+
+####################################################################################################
+"""Run this file"""
+
+def main():
+    print("Hello World")
+
+if __name__ == "__main__":
+    main() # makes sure this only runs if you run *this* file, not if this file is imported somewhere else
